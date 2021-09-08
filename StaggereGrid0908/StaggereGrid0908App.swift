ae810adf6a0d9912c347fa65426934a6e0b4fce3
张亚飞 <672233580@qq.com>
2021-09-08 10:55:04 +0800

    Initial Commit

---
+++ StaggereGrid0908/StaggereGrid0908App.swift
@@ -0,0 +1,17 @@
+//
+//  StaggereGrid0908App.swift
+//  StaggereGrid0908
+//
+//  Created by 张亚飞 on 2021/9/8.
+//
+
+import SwiftUI
+
+@main
+struct StaggereGrid0908App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
